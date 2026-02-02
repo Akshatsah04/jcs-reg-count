481c20fda154fefb43edc7a9b3170ddf364ffe08
Akshatsah04 <iamakshatsah@gmail.com>
2026-02-03 01:30:16 +0530

    till 7

--- src/data/jcs.js
+++ src/data/jcs.js
@@ -1,6 +1,6 @@
 export const JUNIOR_COMMITTEE_MEMBERS = [
   { name: "Akshita Sinha", registrations: 1 },
-  { name: "Ananya Singh", registrations: 0 },
+  { name: "Ananya Singh", registrations: 1 },
   { name: "Aparna", registrations: 0 },
   { name: "Manasvi Bansal", registrations: 0 },
   { name: "Nehal Sharma", registrations: 0 },
